20b51d08a94c31d085a70a02dd97924dd87d440a
sawarn69 <sawarn69@gmail.com>
2017-02-06 17:36:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,29 @@ var button= document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
     // make a request to the counter endpoint
+    
     //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState===200) {
+            var counter=request.responseText;
+        
+    
     //render the variable in the correct span
-    counter=counter + 1;
+    
     var span= document.getElementById('count');
 span,innerHTML = counter.toString();
+}
+}
+};
+var nameInput=document.getElementId('name');
+var name = nameInput.value;
+var submit=document.GetElementById('submit_btn');
+submit.onclick=function() {
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0; i<name.length; i++){
+        list+= 'li' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
 };
