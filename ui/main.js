6a6b8dbde83b8aa37299ae1b1d470dc92fd0d9a2
sawarn69 <sawarn69@gmail.com>
2017-02-06 15:37:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML='New value';
 // move an image
 var img=document.getElementById('madi');
 img.onclick=function() {
-    img.style.marginrightt='100px';
+    img.style.marginright='100px';
 };
