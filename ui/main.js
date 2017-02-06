f68d6a6091d0e6babaf602cbe87cb315c0928d21
sawarn69 <sawarn69@gmail.com>
2017-02-06 15:47:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 // move an image
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight() {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick=function() {
     var interval = setInterval(moveRight, 100);
 };
